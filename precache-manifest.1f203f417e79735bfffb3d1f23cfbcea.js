7a77025759539b0a7d6e8deb53b19513a6bf3d0a
parkyolo <20181621@kookmin.ac.kr>
2020-03-05 02:15:43 +0900

    Updates

---
+++ precache-manifest.1f203f417e79735bfffb3d1f23cfbcea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8824b9c194ca5bdb3b96cb4cafffe7e7",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "7da6e29a6b2b7fed7e25",
+    "url": "/movie_app_2020/static/css/main.6b6cb531.chunk.css"
+  },
+  {
+    "revision": "d533983d33250094a92f",
+    "url": "/movie_app_2020/static/js/2.a4b0d37e.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/movie_app_2020/static/js/2.a4b0d37e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7da6e29a6b2b7fed7e25",
+    "url": "/movie_app_2020/static/js/main.66a0d02e.chunk.js"
+  },
+  {
+    "revision": "dae7d88a09343f6b5f3a",
+    "url": "/movie_app_2020/static/js/runtime-main.d31caeea.js"
+  }
+]);
